ca1d13660627c69275a47f4328e405cc90c5af85
NorikiKunugi <noririkki@gmail.com>
2020-05-04 15:52:38 +0900

    create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { Helmet } from 'react-helmet';
+import Copyright from './components/layout/Copyright';
+import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './styles/theme';
 
 function App() {
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  // const theme = React.useMemo(
+  //   () =>
+  //     createMuiTheme({
+  //       palette: {
+  //         type: prefersDarkMode ? 'dark' : 'light',
+  //       },
+  //     }),
+  //   [prefersDarkMode]
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Helmet
+          title="One Night Werewolf Online"
+          meta={[
+            {
+              name: 'viewport',
+              content: 'width=device-width, initial-scale=1, user-scalable=no',
+            },
+          ]}
+        />
+        <Switch></Switch>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/functions';
+import 'firebase/auth';
+
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FB_DATABASE_URL,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+firebase.functions();
+
+export default firebase;
